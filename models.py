13fc20ea09b4e8576addfa1cbeb30a3c6708a19b
Tofeni <tofenimarius@yahoo.com>
2021-09-27 20:18:02 +0300

    encrypt db

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-from app import db
+from app import db, app
 import uuid
+from sqlalchemy_utils import StringEncryptedType
 # Database ORMs
+key = app.config['SECRET_KEY']
 
 ItemNotification = db.Table('ItemDetail',
                             db.Column('id', db.Integer, primary_key=True),
@@ -11,16 +13,16 @@ ItemNotification = db.Table('ItemDetail',
 class User(db.Model):
     __tablename__ = 'user'
     user_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(70), unique=True)
-    password = db.Column(db.String(80))
-    phone = db.Column(db.String(50))
-    location = db.Column(db.String(100))
-    push_token = db.Column(db.String(200))
-    last_login = db.Column(db.DateTime)
-    ta_accept = db.Column(db.Boolean, default=False)
-    os_type = db.Column(db.String(200))
-    language = db.Column(db.String(200))
+    name = db.Column(StringEncryptedType(db.String(100), key))
+    email = db.Column(StringEncryptedType(db.String(70), key), unique=True)
+    password = db.Column(StringEncryptedType(db.String(80), key))
+    phone = db.Column(StringEncryptedType(db.String(50), key))
+    location = db.Column(StringEncryptedType(db.String(100), key))
+    push_token = db.Column(StringEncryptedType(db.String(200), key))
+    last_login = db.Column(StringEncryptedType(db.DateTime, key))
+    ta_accept = db.Column(StringEncryptedType(db.Boolean, key), default=False)
+    os_type = db.Column(StringEncryptedType(db.String(200), key))
+    language = db.Column(StringEncryptedType(db.String(200), key))
     image = db.relationship("Image", back_populates="user")
     notification = db.relationship("Notification", secondary=ItemNotification, back_populates="user")
     order = db.relationship("Order", back_populates="user")
@@ -31,38 +33,38 @@ class Image(db.Model):
     __tablename__ = 'image'
     image_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
     user_id = db.Column(db.String, db.ForeignKey('user.user_id'), nullable=False)
-    image = db.Column(db.String, nullable=False)
-    orientation = db.Column(db.String, nullable=False)
-    disease = db.Column(db.String(80))
-    treatment = db.Column(db.String(100))
-    created_data = db.Column(db.DateTime)
-    updated_data = db.Column(db.DateTime)
-    category = db.Column(db.String(80), nullable=False)
+    image = db.Column(StringEncryptedType(db.String, key), nullable=False)
+    orientation = db.Column(StringEncryptedType(db.String, key), nullable=False)
+    disease = db.Column(StringEncryptedType(db.String(80), key))
+    treatment = db.Column(StringEncryptedType(db.String(100), key))
+    created_data = db.Column(StringEncryptedType(db.DateTime, key))
+    updated_data = db.Column(StringEncryptedType(db.DateTime, key))
+    category = db.Column(StringEncryptedType(db.String(80), key), nullable=False)
     user = db.relationship("User", back_populates="image")
 
 
 class Notification(db.Model):
     __tablename__ = 'notification'
     notification_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    text = db.Column(db.String)
-    data = db.Column(db.DateTime)
-    read_flag = db.Column(db.Boolean, nullable=False)
-    category = db.Column(db.String(80))
+    title = db.Column(StringEncryptedType(db.String(80), key), nullable=False)
+    text = db.Column(StringEncryptedType(db.String, key))
+    data = db.Column(StringEncryptedType(db.DateTime, key))
+    read_flag = db.Column(StringEncryptedType(db.Boolean, key), nullable=False)
+    category = db.Column(StringEncryptedType(db.String(80), key))
     user = db.relationship("User", secondary=ItemNotification, back_populates="notification")
 
 
 class Product(db.Model):
     __tablename__ = 'product'
     product_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
-    producer = db.Column(db.String(80), nullable=False)
-    name = db.Column(db.String(80), nullable=False)
-    image = db.Column(db.String)
-    description = db.Column(db.String)
-    price = db.Column(db.Float)
-    provider = db.Column(db.String(80))
-    stock = db.Column(db.Integer)
-    stock_flag = db.Column(db.Boolean, nullable=False)
+    producer = db.Column(StringEncryptedType(db.String(80), key), nullable=False)
+    name = db.Column(StringEncryptedType(db.String(80), key), nullable=False)
+    image = db.Column(StringEncryptedType(db.String, key))
+    description = db.Column(StringEncryptedType(db.String, key))
+    price = db.Column(StringEncryptedType(db.Float, key))
+    provider = db.Column(StringEncryptedType(db.String(80), key))
+    stock = db.Column(StringEncryptedType(db.Integer, key))
+    stock_flag = db.Column(StringEncryptedType(db.Boolean, key), nullable=False)
     product_details = db.relationship("Product_Details", back_populates="product")
     ordered_products = db.relationship("Ordered_Products", back_populates="product")
 
@@ -71,11 +73,11 @@ class Product_Details(db.Model):
     __tablename__ = 'product_details'
     product_details_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
     product_id = db.Column(db.String, db.ForeignKey('product.product_id'), nullable=False)
-    field = db.Column(db.String(80), nullable=False)
-    pests = db.Column(db.String(80), nullable=False)
-    dose_Ha = db.Column(db.Float)
-    dose_1_m2 = db.Column(db.Float)
-    break_time = db.Column(db.String(80))
+    field = db.Column(StringEncryptedType(db.String(80), key), nullable=False)
+    pests = db.Column(StringEncryptedType(db.String(80), key), nullable=False)
+    dose_Ha = db.Column(StringEncryptedType(db.Float, key))
+    dose_1_m2 = db.Column(StringEncryptedType(db.Float, key))
+    break_time = db.Column(StringEncryptedType(db.String(80), key))
     product = db.relationship("Product", back_populates="product_details")
 
 
@@ -83,12 +85,12 @@ class Order(db.Model):
     __tablename__ = 'order'
     order_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
     user_id = db.Column(db.String, db.ForeignKey('user.user_id'), nullable=False)
-    total_price = db.Column(db.Float, nullable=False)
-    status = db.Column(db.String(80), nullable=False)
-    created_data = db.Column(db.DateTime)
+    total_price = db.Column(StringEncryptedType(db.Float, key), nullable=False)
+    status = db.Column(StringEncryptedType(db.String(80), key), nullable=False)
+    created_data = db.Column(StringEncryptedType(db.DateTime, key))
     orderdetails_shipping_id = db.Column(db.Integer, db.ForeignKey('orderdetails.orderdetails_id'), nullable=False)
     orderdetails_billing_id = db.Column(db.Integer, db.ForeignKey('orderdetails.orderdetails_id'), nullable=False)
-    payment_type = db.Column(db.String(80))
+    payment_type = db.Column(StringEncryptedType(db.String(80), key))
     user = db.relationship("User", back_populates="order")
     ordered_products = db.relationship("Ordered_Products", back_populates="order")
 
@@ -98,7 +100,7 @@ class Ordered_Products(db.Model):
     ordered_products_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
     order_id = db.Column(db.String, db.ForeignKey('order.order_id'), nullable=False)
     product_id = db.Column(db.String, db.ForeignKey('product.product_id'), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(StringEncryptedType(db.Integer, key), nullable=False)
     order = db.relationship("Order", back_populates="ordered_products")
     product = db.relationship("Product", back_populates="ordered_products")
 
@@ -107,27 +109,27 @@ class Orderdetails(db.Model):
     __tablename__ = 'orderdetails'
     orderdetails_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
     user_id = db.Column(db.String, db.ForeignKey('user.user_id'), nullable=False)
-    order_type = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(70))
-    first_name = db.Column(db.String(50))
-    second_name = db.Column(db.String(50))
-    phone = db.Column(db.String(50))
-    county = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    street = db.Column(db.String(50))
-    number = db.Column(db.String(30))
-    block = db.Column(db.String(30))
-    stairs = db.Column(db.String(30))
-    apartment = db.Column(db.String(30))
+    order_type = db.Column(StringEncryptedType(db.String(20), key), nullable=False)
+    email = db.Column(StringEncryptedType(db.String(70), key))
+    first_name = db.Column(StringEncryptedType(db.String(50), key))
+    second_name = db.Column(StringEncryptedType(db.String(50), key))
+    phone = db.Column(StringEncryptedType(db.String(50), key))
+    county = db.Column(StringEncryptedType(db.String(50), key))
+    city = db.Column(StringEncryptedType(db.String(50), key))
+    street = db.Column(StringEncryptedType(db.String(50), key))
+    number = db.Column(StringEncryptedType(db.String(30), key))
+    block = db.Column(StringEncryptedType(db.String(30), key))
+    stairs = db.Column(StringEncryptedType(db.String(30), key))
+    apartment = db.Column(StringEncryptedType(db.String(30), key))
     user = db.relationship("User", back_populates="orderdetails")
 
 
 class Phyto(db.Model):
     __tablename__ = 'phyto'
     phyto_id = db.Column(db.String, default=lambda: uuid.uuid4().hex, primary_key=True)
-    location = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    county = db.Column(db.String(100))
-    email = db.Column(db.String(70))
-    phone = db.Column(db.String(50))
-    hours = db.Column(db.String(100))
+    location = db.Column(StringEncryptedType(db.String(100), key))
+    name = db.Column(StringEncryptedType(db.String(100), key))
+    county = db.Column(StringEncryptedType(db.String(100), key))
+    email = db.Column(StringEncryptedType(db.String(70), key))
+    phone = db.Column(StringEncryptedType(db.String(50), key))
+    hours = db.Column(StringEncryptedType(db.String(100), key))
